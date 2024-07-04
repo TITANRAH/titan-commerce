4f291976c1ebcc9992012effd6faa900cbc2d3eb
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-07-04 14:24:11 -0400

    hola

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -3,31 +3,35 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            aaaaaaaa
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Need help customizing your store?
-          </Heading>
-        </span>
-        <a
-          href="https://funkyton.com/tutorial-customize-your-medusajs-webshop-storefron/"
-          target="_blank"
-        >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the
-          </h1>
-        </a>
-      </div>
+    // <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    //   <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+    //     <span>
+    //       <Heading
+    //         level="h1"
+    //         className="text-3xl leading-10 text-ui-fg-base font-normal"
+    //       >
+    //         aaaaaaaa
+    //       </Heading>
+    //       <Heading
+    //         level="h2"
+    //         className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+    //       >
+    //         Need help customizing your store?
+    //       </Heading>
+    //     </span>
+    //     <a
+    //       href="https://funkyton.com/tutorial-customize-your-medusajs-webshop-storefron/"
+    //       target="_blank"
+    //     >
+    //       <h1 style={{ textDecoration: "underline" }}>
+    //         Visit the
+    //       </h1>
+    //     </a>
+    //   </div>
+    // </div>
+
+    <div>
+      Hola
     </div>
   )
 }
